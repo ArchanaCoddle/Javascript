38d55048d2aad649b5e9c2408d36a28cd6284455
archanaCoddle <archana.sk@coddletech.com>
2023-08-02 10:00:09 +0530

    first

---
+++ Assignment_1/script.js
@@ -0,0 +1,21 @@
+var num=25;
+var msg="hello";
+var bool=true;
+var ud_variable;
+var null_variable=null;
+let arr=[1,2,3];
+let object={id:11,name:"abc"};
+console.log("num is a "+typeof num);
+console.log("msg is a "+typeof msg);
+console.log("bool is a "+typeof bool);
+console.log("ud_variable is an "+typeof ud_variable);
+console.log("null_variable is a "+typeof null_variable);
+console.log("arr is an "+typeof arr);
+console.log("object is an "+typeof object);
+
+let text_notemp=num+","+msg+","+bool+","+ud_variable+","+null_variable+","+arr+","+object;
+let text_temp=`${num},${msg},${bool},${ud_variable},${null_variable},${arr},${object}`
+console.log("Without template literals - "+text_notemp)
+console.log("With template literals - "+text_temp)
+
+alert("Hello welcome to this page");
