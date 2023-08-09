b84ba207f85f0c433b9e5a083071bb73e3faff15
archanaCoddle <archana.sk@coddletech.com>
2023-08-09 12:21:45 +0530

    fourth

--- Assignment_4/script.js
+++ Assignment_4/script.js
@@ -51,8 +51,8 @@ formSubmit.addEventListener('click', () => {
   document.cookie = `company=${pCompany}; expires=Wed, 9 aug 2023 24:00:00 UTC; path=/`;
 });
 // 4.Redirect to the homepage of google from the console.
-// const url = 'https://www.google.com';
-// window.location.href = url;
+const url = 'https://www.google.com';
+window.location.href = url;
 
 //5. Create a div with background color red, create buttons
 // to hide the div
